59dfc35517d1bac9abc6d42a60effe73333550bf
Agus Darmawan <5024211070@mhs.its.ac.id>
2024-05-28 20:19:49 +0700

    feat: add login registration view and input component

--- ThreadsClone/Core/Autentication/View/RegistrationView.swift
+++ ThreadsClone/Core/Autentication/View/RegistrationView.swift
@@ -1,5 +1,5 @@
 //
-//  LoginView.swift
+//  RegistrationView.swift
 //  ThreadsClone
 //
 //  Created by Agus Darmawan on 28/05/24.
@@ -7,38 +7,34 @@
 
 import SwiftUI
 
-struct LoginView: View {
+struct RegistrationView: View {
     @State private var email = ""
     @State private var password = ""
+    @State private var fullname = ""
+    @State private var username = ""
     var body: some View {
-        NavigationStack{
-            VStack{
+        NavigationStack {
+            VStack {
                 Spacer()
                 Image("threads-app-icon")
                     .resizable()
                     .scaledToFit()
-                    .frame(width: 120,height: 120)
+                    .frame(width: 120, height: 120)
                     .padding()
                 VStack {
-                    TextField("Enter your email",text: $email)
+                    TextField("Enter your email", text: $email)
+                        .autocapitalization(.none)
                         .modifier(ThreadsTextViewModifier())
-                    SecureField("Enter your password", text:$password)
+                    SecureField("Enter your password", text: $password)
+                        .modifier(ThreadsTextViewModifier())
+                    TextField("Enter your fullname", text: $fullname)
+                        .modifier(ThreadsTextViewModifier())
+                    TextField("Enter your username", text: $username)
                         .modifier(ThreadsTextViewModifier())
                 }
-                NavigationLink {
-                Text("Forgot password" )
+                Button {
                 } label: {
-                    Text("Forgot Password?")
-                        .font (.footnote)
-                        .fontWeight(.semibold)
-                        .padding(. top)
-                        .padding(.trailing, 28)
-                        .foregroundColor(.black)
-                        .frame(maxWidth: .infinity, alignment: .trailing)
-                }
-                Button{
-                }label: {
-                    Text("Login")
+                    Text("Register")
                         .font(.subheadline)
                         .fontWeight(.semibold)
                         .foregroundColor(.white)
@@ -46,14 +42,17 @@ struct LoginView: View {
                         .background(.black)
                         .cornerRadius(8)
                 }
+                .padding(.vertical)
+                
                 Spacer()
                 
                 Divider()
-                Button {
-                } label:{
-                    HStack(spacing : 4) {
-                        Text("Don't have an account?")
-                         Text ("Sign Up")
+                NavigationLink {
+                    LoginView()  // Navigate to LoginView
+                } label: {
+                    HStack(spacing: 4) {
+                        Text("Already have an account?")
+                        Text("Sign In")
                             .fontWeight(.semibold)
                     }
                     .foregroundColor(.black)
@@ -65,8 +64,8 @@ struct LoginView: View {
     }
 }
 
-struct LoginView_Previews: PreviewProvider {
+struct RegistrationView_Previews: PreviewProvider {
     static var previews: some View {
-        LoginView()
+        RegistrationView()
     }
 }
---
+++ ThreadsClone/Utils/ViewModifier/ThreadsTextViewModifier.swift
@@ -0,0 +1,19 @@
+//
+//  ThreadsTextViewModifier.swift
+//  ThreadsClone
+//
+//  Created by Agus Darmawan on 28/05/24.
+//
+
+import SwiftUI
+
+struct ThreadsTextViewModifier: ViewModifier{
+    func body(content: Content) -> some View {
+    content
+            .font(.subheadline)
+            .padding(12)
+            .background(Color(.systemGray6))
+            .cornerRadius(10)
+            .padding(.horizontal, 24)
+    }
+}
