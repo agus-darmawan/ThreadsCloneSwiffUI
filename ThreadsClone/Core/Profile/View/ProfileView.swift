502249b1f17760103fa33f5535fb8ef332828a09
Agus Darmawan <5024211070@mhs.its.ac.id>
2024-05-29 11:19:23 +0700

    feat: add user profile

--- ThreadsClone/Core/Profile/View/ProfileView.swift
+++ ThreadsClone/Core/Profile/View/ProfileView.swift
@@ -9,7 +9,38 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text("Profile View")
+        ScrollView (showsIndicators: false) {
+            VStack(spacing:20) {
+                HStack(alignment: .top) {
+                    VStack(alignment: .leading, spacing: 12) {
+                        VStack (alignment: .leading) {
+                            Text("Agus Darmawan")
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                            Text("agusdarmwn")
+                                .font(.subheadline)
+                        }
+                        Text("Lorem ipsum dolor sit amet, consectetur")
+                            .font(.footnote)
+                        Text("1K follower")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                    }
+                    Spacer()
+                    CircularProfileImageView()
+                }
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text("Follow")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(width: 352, height: 32)
+                        .background(.black)
+                        .cornerRadius(8)
+                })
+            }
+        }
+        .padding(.horizontal)
     }
 }
 
