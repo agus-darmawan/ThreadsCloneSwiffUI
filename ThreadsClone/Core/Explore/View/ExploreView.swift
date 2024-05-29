de3cf32437445def32c96eccef81b32f2f5f9cf2
Agus Darmawan <5024211070@mhs.its.ac.id>
2024-05-29 10:33:19 +0700

    feat: add explore view

--- ThreadsClone/Core/Explore/View/ExploreView.swift
+++ ThreadsClone/Core/Explore/View/ExploreView.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct ExploreView: View {
+    @State private var searchText = ""
     var body: some View {
-        Text("Hello Explore")
+        NavigationStack {
+            ScrollView{
+                LazyVStack {
+                    ForEach(0 ... 10, id :\.self){thread in
+                        VStack {
+                            HStack {
+                                Image("user-profile")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 40,height: 40)
+                                    .clipShape(Circle())
+                                VStack(alignment: .leading){
+                                    Text("niwayannia21")
+                                        .fontWeight(.semibold)
+                                    Text("Wayan Nia")
+                                }
+                                .font(.footnote)
+                                
+                                Spacer()
+                                
+                                Text("Follow")
+                                    .font(.subheadline)
+                                    .fontWeight(.semibold)
+                                    .frame(width: 100, height: 32)
+                                    .overlay{
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .stroke(Color(.systemGray4),lineWidth: 1)
+                                        
+                                    }
+                            }
+                            .padding(.horizontal)
+                            
+                            Divider()
+                        }
+                        .padding(.vertical,4)
+                    }
+                }
+            }
+            .navigationTitle("Search")
+            .searchable(text: $searchText, prompt: "Search")
+            .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
+        }
     }
 }
 
