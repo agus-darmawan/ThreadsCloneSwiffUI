fd93a16432d365e39b43a4fbd5ff07d5a9f468b1
Agus Darmawan <5024211070@mhs.its.ac.id>
2024-06-01 11:04:36 +0700

    feat: add user profile to firestore

---
+++ ThreadsClone/Core/Profile/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  ProfileViewModel.swift
+//  ThreadsClone
+//
+//  Created by Agus Darmawan on 01/06/24.
+//
+
+import Foundation
+import Combine
+
+class ProfileViewModel: ObservableObject {
+    @Published var currentUser: User?
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        setupSubscriber()
+    }
+    
+    private func setupSubscriber() {
+        UserService.shared.$currentUser
+            .sink { [weak self] user in
+                self?.currentUser = user
+                print("User in view model combine is \(String(describing: user))")
+            }
+            .store(in: &cancellables)
+    }
+}
---
+++ ThreadsClone/Services/UserService.swift
@@ -0,0 +1,26 @@
+//
+//  UserService.swift
+//  ThreadsClone
+//
+//  Created by Agus Darmawan on 01/06/24.
+//
+
+import Firebase
+import FirebaseFirestoreSwift
+
+class UserService {
+    @Published var currentUser: User?
+    
+    static let shared = UserService()
+    
+    init(){
+        Task {try await fetchCurrentUser()}
+    }
+    @MainActor
+    func fetchCurrentUser() async throws {
+        guard let uid = Auth.auth().currentUser?.uid else {return}
+        let snapshoot = try await Firestore.firestore().collection("users").document(uid).getDocument()
+        let user = try snapshoot.data(as: User.self)
+        self.currentUser = user
+    }
+}
